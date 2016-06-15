96f3f5dfbf53bbd6b7e472f3bf5459a335b77b40
SuperbiaYang <yhmShanghai@gmail.com>
2016-06-15 11:36:43 +0800

    Unfinished version, polish rotate

--- app/src/main/java/processor/Geometry.java
+++ app/src/main/java/processor/Geometry.java
@@ -1,55 +1,58 @@
 package processor;
 
-import android.util.Log;
-
 import superbiayang.imageprocessing.PicInfo;
 
 /**
  * Created by SuperbiaYang on 2016/6/11.
  */
 public class Geometry {
+
     public static PicInfo nearestNeighborRotate(PicInfo pic, int degree) {
         int width = pic.getWidth();
         int height = pic.getHeight();
         int[] src = pic.getPixels();
-        int newWidth = 0;
-        int newHeight = 0;
-        int newCenter_x = 0;
-        int newCenter_y = 0;
+        int newWidth, newHeight;
 
-        int n = (int) (degree / 360);
+        int n = degree / 360;
         degree = degree - n * 360;
         if (degree < 0) {
             degree += 360;
         }
         double ang = Math.toRadians(degree);
 
+        double sin = Math.sin(ang);
+        double cos = Math.cos(ang);
+
         if (degree == 180 || degree == 0 || degree == 360) {
             newWidth = width;
             newHeight = height;
         } else if (degree == 90 || degree == 270) {
+            //noinspection SuspiciousNameCombination
             newWidth = height;
+            //noinspection SuspiciousNameCombination
             newHeight = width;
         } else {
-            newWidth = (int) (width * Math.abs(Math.cos(ang)) + height * Math.abs(Math.sin(ang)));
-            newHeight = (int) (width * Math.abs(Math.sin(ang)) + height * Math.abs(Math.cos(ang)));
+            newWidth = (int) (width * Math.abs(cos) + height * Math.abs(sin));
+            newHeight = (int) (width * Math.abs(sin) + height * Math.abs(cos));
         }
-
-        newCenter_x = newWidth / 2;
-        newCenter_y = newHeight / 2;
 
         int[] ret = new int[newWidth * newHeight];
 
-        Log.i("w,h", newWidth + "," + newHeight + "," + width + "," + height + "," + src.length + "," + ret.length);
-        for (int y = -newHeight / 2; y < newHeight / 2; y++) {
-            for (int x = -newWidth / 2; x < newWidth / 2; x++) {
-                int original_x = (int) (x * Math.cos(ang) - y * Math.sin(ang));
-                int original_y = (int) (x * Math.sin(ang) + y * Math.cos(ang));
-                original_x += width / 2;
-                original_y += height / 2;
-                int cur_x = x + newWidth / 2;
-                int cur_y = y + newHeight / 2;
-                //Log.i("x y", cur_x + "," + cur_y + "," + original_x + "," + original_y);
+        int newCentral_x = newWidth / 2;
+        int newCentral_y = newHeight / 2;
+
+        int central_x = width / 2;
+        int central_y = height / 2;
+
+        int i = 0;
+        for (int y = -newCentral_y; y < newCentral_y; y++) {
+            for (int x = -newCentral_x; x < newCentral_x; x++) {
+                int original_x = (int) (x * cos - y * sin);
+                int original_y = (int) (x * sin + y * cos);
+                original_x += central_x;
+                original_y += central_y;
+                int cur_x = x + newCentral_x;
+                int cur_y = y + newCentral_y;
 
                 if (original_x < 0 || original_x >= width || original_y < 0 || original_y >= height) {
                     ret[cur_x + cur_y * newWidth] = 0xFF000000;
